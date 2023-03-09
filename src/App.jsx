7ae3a056dbf79c1b0510f86f2c69af57e1ad5f2b
Smayan Nirantare <smayannirantare5@gmail.com>
2023-03-08 20:34:51 -0800

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import * as onnx from "onnxjs";
+import React, { useEffect } from "react";
+import model from "./models/model.onnx";
+
+function App() {
+	async function inference() {
+		const session = new onnx.InferenceSession();
+		await session.loadModel(model);
+		const inputTensor = new onnx.Tensor(new Float32Array(784), "float32", [1, 784]);
+		const outputMap = await session.run([inputTensor]);
+		const outputTensor = outputMap.values().next().value;
+		console.log(outputTensor.data);
+	}
+	return <div></div>;
+}
+
+export default App;
